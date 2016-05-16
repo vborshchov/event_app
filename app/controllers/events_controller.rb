63d00deb190283ebc85d15d9bbae534fc8b436e7
Victor Borshchov <victor.borshchov@gmail.com>
2016-05-16 16:05:19 +0300

    Adds events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,11 +4,33 @@ class EventsController < ApplicationController
   end
 
   def new
+    if user_signed_in?
+      @event = Event.new()
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def create
+    @event = Event.new(event_params.merge(user_id: current_user.id)) if user_signed_in?
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+        format.json { render json: @event, status: :created, location: @event }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
+    @event = Event.find(params[:id])
   end
+
+  private
+   def event_params
+      params.require(:event).permit(:name, :address, :description, :event_date, :event_time, :min_users, :max_users, :user_id)
+    end
 end
