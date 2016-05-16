05b7da3a34ca2e1d4ea84bbaff76703110256b8f
Victor Borshchov <victor.borshchov@gmail.com>
2016-05-16 11:49:22 +0300

    Adds self-referential association subscribers for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_many :events
   has_many :categories
+  has_many :subscriptions
+  has_many :subscribers, :through => :subscriptions
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516075855) do
+ActiveRecord::Schema.define(version: 20160516083816) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -38,6 +38,15 @@ ActiveRecord::Schema.define(version: 20160516075855) do
 
   add_index "events", ["category_id"], name: "index_events_on_category_id"
   add_index "events", ["user_id"], name: "index_events_on_user_id"
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "subscriber_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "subscriptions", ["user_id"], name: "index_subscriptions_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
