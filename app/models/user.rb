3b9c70c7333343b4c362c22e0fc2ca7c03ea2393
Victor Borshchov <victor.borshchov@gmail.com>
2016-05-16 12:39:39 +0300

    Adds many to many relationship between events and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :events
+  # has_many :events
   has_many :categories
+  has_and_belongs_to_many :events
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516075855) do
+ActiveRecord::Schema.define(version: 20160516092641) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -38,6 +38,23 @@ ActiveRecord::Schema.define(version: 20160516075855) do
 
   add_index "events", ["category_id"], name: "index_events_on_category_id"
   add_index "events", ["user_id"], name: "index_events_on_user_id"
+
+  create_table "events_users", id: false, force: :cascade do |t|
+    t.integer "event_id"
+    t.integer "user_id"
+  end
+
+  add_index "events_users", ["event_id"], name: "index_events_users_on_event_id"
+  add_index "events_users", ["user_id"], name: "index_events_users_on_user_id"
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "subscriber_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "subscriptions", ["user_id"], name: "index_subscriptions_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
