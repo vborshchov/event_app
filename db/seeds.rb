906f409d834ef089739b3ddf92c55fde0cc05549
Victor Borshchov <victor.borshchov@gmail.com>
2016-05-17 11:46:08 +0300

    Makes seed file for users and events

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = User.create(
+  [
+    {
+      name: "Alice",
+      email: "alice@example.com",
+      phone: "555-555-1234",
+      skype: "alice",
+      about: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus commodi dicta, sint architecto ad illum voluptatum praesentium saepe. Laborum sunt nobis a ipsa officiis unde voluptatem nulla, quaerat sequi eum?",
+      password: "alicealice",
+      password_confirmation: "alicealice"
+    },
+    {
+      name: "Bob",
+      email: "bob@example.com",
+      phone: "555-555-4321",
+      skype: "bob",
+      about: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus commodi dicta, sint architecto ad illum voluptatum praesentium saepe. Laborum sunt nobis a ipsa officiis unde voluptatem nulla, quaerat sequi eum?",
+      password: "bobbobbob",
+      password_confirmation: "bobbobbob"
+    }
+  ]
+) 
+
+users.each do |user|
+  Event.create(
+    [
+      {
+        name: "dolor #{user.name}",
+        address: "Copenhagen",
+        min_users: 2,
+        max_users: 5,
+        user_id: user.id,
+        category: Category.first
+      },
+      {
+        name: "amet #{user.name}",
+        address: "Cherkassy",
+        min_users: 3,
+        max_users: 10,
+        user_id: user.id,
+        category: Category.last
+      }
+    ]
+  )
+end
