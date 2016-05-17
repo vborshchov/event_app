dcfa833ac390ba06bb5308f2aee6a90c8d655ed7
Victor Borshchov <victor.borshchov@gmail.com>
2016-05-17 12:14:46 +0300

    Makes event page

--- app/helpers/subscribe_button_helper.rb
+++ app/helpers/subscribe_button_helper.rb
@@ -2,7 +2,7 @@ module SubscribeButtonHelper
   def subscribe_button_for item
     if user_signed_in? && item.creator != current_user
       unless item.subscribers.include? current_user
-        link_to "Subscribe", subscribe_path(item), class: "btn btn-primary", method: :post
+        link_to "Subscribe", subscribe_path(item), class: "btn btn-primary btn-lg", method: :post
       end
     end
   end
